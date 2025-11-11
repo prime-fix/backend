c21d988848f69d9cdf6b240550b4dc0056ba0de8
Cesar- <legende.e@hotmail.com>
2025-11-10 21:40:14 -0500

    feat: add Swagger configuration URL for development and production environments

--- src/main/java/pe/edu/upc/prime/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/pe/edu/upc/prime/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -8,14 +8,20 @@ import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 /**
  * Configuration class for OpenAPI documentation.
  */
 @Configuration
 public class OpenApiConfiguration {
+
+    @Value("${swagger.server.url}")
+    private String swaggerServerUrl;
 
     /**
      * Configures the OpenAPI documentation for the Prime Fix Platform API.
@@ -27,18 +33,20 @@ public class OpenApiConfiguration {
         final String securitySchemeName = "bearerAuth";
 
         return new OpenAPI()
-                .addServersItem(new Server()
-                        .url("https://cadw4v2ry8.us-east-2.awsapprunner.com")
-                        .description("Despliegue en AWS App Runner (Producción)"))
+                .servers(List.of(
+                        new Server()
+                                .url(swaggerServerUrl)
+                                .description("Active server environment for Prime Fix Platform")
+                ))
                 .info(new Info()
                         .title("Prime Fix Platform API")
-                        .description("Documentación de la API REST de Prime Fix Platform.")
+                        .description("REST API documentation for Prime Fix Platform.")
                         .version("v1.0.0")
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("https://springdoc.org")))
                 .externalDocs(new ExternalDocumentation()
-                        .description("Repositorio oficial")
+                        .description("Official repository")
                         .url("https://github.com/prime-fix/backend"))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new Components()
