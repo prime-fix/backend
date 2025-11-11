3a7df85e58ddc4f18284ef8d93e0c9969c2cbddb
Cesar- <legende.e@hotmail.com>
2025-11-10 21:00:25 -0500

    feat: enhance OpenAPI configuration and add CORS settings

--- src/main/java/pe/edu/upc/prime/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/pe/edu/upc/prime/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -1,7 +1,5 @@
 package pe.edu.upc.prime.platform.shared.infrastructure.documentation.openapi.configuration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.v3.core.jackson.ModelResolver;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -9,6 +7,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,38 +18,35 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfiguration {
 
     /**
-     * Configures the OpenAPI documentation for the Learning Platform API.
+     * Configures the OpenAPI documentation for the Prime Fix Platform API.
      *
-     * @return an OpenAPI object with the configured settings
+     * @return the OpenAPI configuration
      */
     @Bean
     public OpenAPI primePlatformOpenApi() {
-        // General configuration
-        var openApi = new OpenAPI();
-        openApi
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .addServersItem(new Server()
+                        .url("https://cadw4v2ry8.us-east-2.awsapprunner.com")
+                        .description("Despliegue en AWS App Runner (Producción)"))
                 .info(new Info()
                         .title("Prime Fix Platform API")
-                        .description("Prime Fix Platform application REST API documentation.")
+                        .description("Documentación de la API REST de Prime Fix Platform.")
                         .version("v1.0.0")
-                        .license(new License().name("Apache 2.0")
+                        .license(new License()
+                                .name("Apache 2.0")
                                 .url("https://springdoc.org")))
                 .externalDocs(new ExternalDocumentation()
-                        .description("Prime Fix Platform Documentation")
-                        .url("https://github.com/prime-fix/backend"));
-
-        // Add security scheme
-    /*final String securitySchemeName = "bearerAuth";
-    openApi.addSecurityItem(new SecurityRequirement()
-            .addList(securitySchemeName))
-        .components(new Components()
-            .addSecuritySchemes(securitySchemeName,
-                new SecurityScheme()
-                    .name(securitySchemeName)
-                    .type(SecurityScheme.Type.HTTP)
-                    .scheme("bearer")
-                    .bearerFormat("JWT")));*/
-
-        // Return the OpenAPI configuration object with all the settings
-        return openApi;
+                        .description("Repositorio oficial")
+                        .url("https://github.com/prime-fix/backend"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
